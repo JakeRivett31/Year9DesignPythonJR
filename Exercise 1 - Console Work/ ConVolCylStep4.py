c0915ca1f54996f9ff754cd08cbab4f037a5d25e
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-09-17 13:13:17 -0400

    Starting Step 4 for Exercise 1

---
+++ Exercise 1 - Console Work/ ConVolCylStep4.py
@@ -0,0 +1,40 @@
+#Input
+#What inputs are needed to calculate the volume of a cylinder?
+print(input("""\nWelcome to the Cylinder Volume Calculator!
+Press enter to begin"""))
+
+height = 1
+
+radius = 1
+
+		
+
+name = str(input("What is your name? "))
+
+
+
+while radius != 0 or height != 0:
+	
+	unit = str(input("\nWhat is the unit you are using? (Enter abbreviated version) "))
+
+	radius = int(input("\nWhat is the radius of the cylinder? "))
+	height = int(input("\nWhat is the height of the cylinder? "))
+
+	#Process
+	#What formula is used to calculate the volume of a cylinder?
+	radiussquared = radius * radius
+	import math
+	pi = math.pi
+	formula = float(pi * radiussquared * height)
+	formula = round(formula,2)
+
+#Output
+#What is important about the output?
+	if radius >= 0 and height >= 0:
+		print("\nHere is your volume " + name + " I hope you enjoyed!")
+		print(str(formula) + str(unit) + "\u00B3")
+	else: 
+		print("\nPlease do not use negative numbers.")
+
+	if radius == 0 or height == 0:
+		print("\nThank you for using the Calculator!")
