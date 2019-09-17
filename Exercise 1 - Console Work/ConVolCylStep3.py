9cecab0bda862b4e32dc0c72c95d32fd66afbdf3
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-09-17 11:58:55 -0400

    Finished Step 3 - while loops, if else statements

--- Exercise 1 - Console Work/ConVolCylStep3.py
+++ Exercise 1 - Console Work/ConVolCylStep3.py
@@ -3,20 +3,35 @@
 print(input("""\nWelcome to the Cylinder Volume Calculator!
 Press enter to begin"""))
 
-name = str(input("What is your name? "))
-unit = str(input("\nWhat is the unit you are using? (Enter abbreviated version) "))
-radius = int(input("\nWhat is the radius of the cylinder? "))
-height = int(input("\nWhat is the height of the cylinder? "))
+height = 1
 
-#Process
-#What formula is used to calculate the volume of a cylinder?
-radiussquared = radius * radius
-import math
-pi = math.pi
-formula = float(pi * radiussquared * height)
-formula = round(formula,2)
+radius = 1
+
+		
+
+name = str(input("What is your name? "))
+
+
+
+while radius != 0 or height != 0:
+	
+	unit = str(input("\nWhat is the unit you are using? (Enter abbreviated version) "))
+
+	radius = int(input("\nWhat is the radius of the cylinder? "))
+	height = int(input("\nWhat is the height of the cylinder? "))
+
+	#Process
+	#What formula is used to calculate the volume of a cylinder?
+	radiussquared = radius * radius
+	import math
+	pi = math.pi
+	formula = float(pi * radiussquared * height)
+	formula = round(formula,2)
 
 #Output
 #What is important about the output?
-print("\nHere is your volume " + name + " I hope you enjoyed!")
-print(str(formula) + str(unit) + "\u00B3")
+	if radius >= 0 and height >= 0:
+		print("\nHere is your volume " + name + " I hope you enjoyed!")
+		print(str(formula) + str(unit) + "\u00B3")
+	else: 
+		print("\nPlease do not use negative numbers.")
