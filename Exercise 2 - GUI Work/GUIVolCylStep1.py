fc6b4c8f3a383a4160378eb11d5b16ba490e536c
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-09-25 14:01:57 -0400

    Finished step 1 of activity 2, getting used to return function

--- Exercise 2 - GUI Work/GUIVolCylStep1.py
+++ Exercise 2 - GUI Work/GUIVolCylStep1.py
@@ -1,44 +1,25 @@
 import math
-def calcVolCylinder():
-	#Input
-	#What inputs are needed to calculate the volume of a cylinder?
-	
-	height = 1
-
-	radius = 1
-
-			
-
-	
-
-	unit = str(input("\nWhat is the unit you are using? (Enter abbreviated version) "))
 
 
-		
-		
+#
 
-	
+def calcVolCylinder(radius, height):
 
-	#Process
-	#What formula is used to calculate the volume of a cylinder?
-	radiussquared = radius * radius
-
-	pi = math.pi
-	formula = float(pi * radiussquared * height)
-	formula = round(formula,2)
-
-	#Output
-	#What is important about the output?
 	if radius >= 0 and height >= 0:
-		
-		print(str(formula) + str(unit) + "\u00B3")
-	else: 
-		print("\nPlease do not use negative numbers.")
+		radiussquared = radius * radius
+		import math
+		pi = math.pi
+		formula = float(pi * radiussquared * height)
+		formula = round(formula,2)
+		return formula
 
-	if radius == 0 or height == 0:
-		print("\nThank you for using the Calculator!")
+	else:
+		return -1
 
-#MAIN CODE
 print("Start Program")
-calcVolCylinder()
+result = calcVolCylinder(3, 4)
+print(result)
 print("End Program")
+
+
+
