163496be04633054470bd20bb7927705d2c45bee
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-10-31 09:51:29 -0400

    Resized image and window

--- Image.py
+++ Image.py
@@ -1,9 +1,11 @@
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image
 import os
 
-root = Tk()
-img = ImageTk.PhotoImage(Image.open("cut.jpg"))
-panel = Label(root, image = img)
-panel.pack(side = "bottom", fill = "both", expand = "yes")
+root = tk.Tk()
+img = ImageTk.PhotoImage(Image.open("SmallCarey.jpg"))
+panel = tk.Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "no")
+root.geometry("120x100+30+30")
 root.mainloop()
+
