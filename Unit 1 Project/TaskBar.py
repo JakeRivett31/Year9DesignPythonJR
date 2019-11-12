8c281f2ede7875ee79c9d2b2c0dd6570be8333ff
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-11-12 16:54:29 -0500

    Took away fullscreen to fix spacing error

--- Unit 1 Project/TaskBar.py
+++ Unit 1 Project/TaskBar.py
@@ -7,19 +7,6 @@ import tkinter as tk
 
 print("Start Program")
 
-class FullScreenApp(object):
-    def __init__(self, master, **kwargs):
-        self.master=master
-        pad=3
-        self._geom='200x200+0+0'
-        master.geometry("{0}x{0}+0+0".format(
-            master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-        master.bind('<Escape>',self.toggle_geom)            
-    def toggle_geom(self,event):
-        geom=self.master.winfo_geometry()
-        print(geom,self._geom)
-        self.master.geometry(self._geom)
-        self._geom=geom
 
 
 root = tk.Tk()
@@ -29,11 +16,12 @@ root.title("Task Bar")
 root.configure(background='#243c6a')
 
 
-canvas = Canvas(root, bg = "#243c6a", highlightthickness=1, height=220, width=300)
+canvas = Canvas(root, bg = "#243c6a", highlightthickness=0, height=100, width=300)
 canvas.grid(row=0, column=0)
 
 photoimage = ImageTk.PhotoImage(file="SmallUCCLogo.png")
-canvas.create_image(150, 150, image=photoimage)
+canvas.create_image(150, 50, image=photoimage)
+
 
 today = date.today()
 
@@ -42,10 +30,23 @@ d2 = today.strftime("%B %d, %Y")
 
 todaysdateLabel = tk.Label(root, text = d2)
 todaysdateLabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "30"))
-todaysdateLabel.grid(row=0, column=1)
+todaysdateLabel.grid(row=0, column=2)
+
+canvas2 = Canvas(root, width=800, height=100)
+canvas2.grid(row=1, column=1)
+
+canvas2.create_rectangle(0, 0, 0, 0,
+    outline="#6578a0", fill="#243c6a", width=0)
+
+btnrun = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=10)
+btnrun.config(fg="#8b0000")
+btnrun.grid(row=1, column=2)
+
+EventLabel = tk.Label(root, text="Event")
+EventLabel.grid(row=1, column=1)
 
 
-app=FullScreenApp(root)
+
 
 root.mainloop()
 
