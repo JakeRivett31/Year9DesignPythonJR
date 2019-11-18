f88af13ec5890ad3482a6adfaffbd22ad06d5098
Jake Rivett <jake.rivett@GCCYJQ5EJ1WK.local>
2019-11-18 17:55:04 -0500

    Can make one new event and write to file

--- Unit 1 Project/TaskBar.py
+++ Unit 1 Project/TaskBar.py
@@ -12,115 +12,132 @@ this is to use Object Oriented Design.
 The effect is that you need to store all variables as lists
 '''
 
-#This will be a temporary data holder 
-data = [] #Creates an empty list
+#VaRIaBLES
+root = tk.Tk()
+
+data = ["","","",""] #Creates an empty list
+
+photocanvas = Canvas(root, bg = "#243c6a", highlightthickness=0, height=100, width=300)
+
+photoimage = ImageTk.PhotoImage(file="SmallUCCLogo.png")
+
+today = date.today()
+
+d2 = today.strftime("%B %d, %Y")
+
+todaysdatelabel = tk.Label(root, text = d2)
+
+taskbarlabel = tk.Label(root, text="Task Bar")
+
+eventcanvas1 = Canvas(root, width=800, height=100)
+
+deletebutton1 = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
+
+eventlabel1 = tk.Label(root, text="Event")
+
+timeanddatelabel1 = tk.Label(root, text="Time and Date")
+
+timeneededlabel1 = tk.Label(root, text="Time Needed")
+
+addneweventbutton = tk.Button(root, text ="Add New Event", fg = "#243c6a", relief = "groove")
+
+file = open("EventDetails.txt","a")
+
+
+#FUNCTIONS
+#creates 4 separate 
+def popupwindow():
+  #There is a nice opportunity for some while loops to error check
+  #You can check the type of a variable using the type command
+  data[0] = simpledialog.askstring("Add New Event", "Event Name:")    
+  data[1] = simpledialog.askstring("Add New Event", "Date (Month, Day, Year):")
+  data[2] = simpledialog.askstring("Add New Event", "Time (Include am or pm):")
+  data[3] = simpledialog.askstring("Add New Event", "Time Needed (In Minutes):")
+  file.write(str(data))
+  eventcanvas = Canvas(root, width=800, height=100)
+  eventcanvas.config(bd=3, relief="ridge", highlightbackground="#6578a0")
+  eventcanvas.grid(row=2, column=1, columnspan=3, pady=10)
+  deletebutton = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
+  deletebutton.config(fg="#8b0000", command = deletetext)
+  deletebutton.grid(row=2, column=4)
+  eventlabel = tk.Label(root, text=data[0])
+  eventlabel.config(font =("Franklin Gothic", "30"), fg="#243c6a")
+  eventlabel.grid(row=2, column=1)
+  timeanddatelabel = tk.Label(root, text=data[1]+"     "+data[2])
+  timeanddatelabel.config(font =("Franklin Gothic", "26"), fg="#243c6a")
+  timeanddatelabel.grid(row=2, column=2)
+  timeneededlabel = tk.Label(root, text=data[3])
+  timeneededlabel.config(font =("Franklin Gothic", "30"), fg="#243c6a")
+  timeneededlabel.grid(row=2, column=3)
 
 
 
+
+
+def deletetext():
+    eventlabel.destroy()
+    timeanddatelabel.destroy()
+    timeneededlabel.destroy()
+    eventcanvas.destroy()
+    deletebutton.destroy()
+
+#deletes whole event bar
+def deletetext1():
+    eventlabel1.destroy()
+    timeanddatelabel1.destroy()
+    timeneededlabel1.destroy()
+    eventcanvas1.destroy()
+    deletebutton1.destroy()
+
+
+
+#WINDOW CONSTRUCTION
 print("Start Program")
 
-root = tk.Tk()
+
 
 root.title("Task Bar")
 
 root.configure(background='#243c6a')
 
+photocanvas.grid(row=0, column=0)
 
-canvas = Canvas(root, bg = "#243c6a", highlightthickness=0, height=100, width=300)
-canvas.grid(row=0, column=0)
-
-photoimage = ImageTk.PhotoImage(file="SmallUCCLogo.png")
-canvas.create_image(150, 50, image=photoimage)
+photocanvas.create_image(150, 50, image=photoimage)
 
 
 
-today = date.today()
-
-d2 = today.strftime("%B %d, %Y")
-
-
-todaysdateLabel = tk.Label(root, text = d2)
-todaysdateLabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "30"))
-todaysdateLabel.grid(row=0, column=4)
+todaysdatelabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "30"))
+todaysdatelabel.grid(row=0, column=4)
 
 
 
-today = date.today()
-
-d2 = today.strftime("%B %d, %Y")
-
-
-todaysdateLabel = tk.Label(root, text = d2)
-todaysdateLabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "30"))
-todaysdateLabel.grid(row=0, column=4)
-
-TaskBarLabel = tk.Label(root, text="Task Bar")
-TaskBarLabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "45"))
-TaskBarLabel.grid(row=0, column=2)
+taskbarlabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "45"))
+taskbarlabel.grid(row=0, column=2)
 
 
-EventCanvas1 = Canvas(root, width=800, height=100)
-EventCanvas1.config(bd=3, relief="ridge", highlightbackground="#6578a0")
-EventCanvas1.grid(row=1, column=1, columnspan=3, pady=10)
 
-#deletes whole event bar
-def deletetext():
-    EventLabel.destroy()
-    TimeAndDateLabel.destroy()
-    TimeNeededLabel.destroy()
-    EventCanvas1.destroy()
-    DeleteButton1.destroy()
-
-DeleteButton1 = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
-DeleteButton1.config(fg="#8b0000", command = deletetext)
-DeleteButton1.grid(row=1, column=4)
-
-EventLabel = tk.Label(root, text="Event")
-EventLabel.config(font =("Franklin Gothic", "30"), fg="#243c6a")
-EventLabel.grid(row=1, column=1)
-
-TimeAndDateLabel = tk.Label(root, text="Time and Date")
-TimeAndDateLabel.config(font =("Franklin Gothic", "30"), fg="#243c6a")
-TimeAndDateLabel.grid(row=1, column=2)
-
-TimeNeededLabel = tk.Label(root, text="Time Needed")
-TimeNeededLabel.config(font =("Franklin Gothic", "30"), fg="#243c6a")
-TimeNeededLabel.grid(row=1, column=3)
-
-EventCanvas2 = Canvas(root, width=800, height=100)
-EventCanvas2.config(bd=3, relief="ridge", highlightbackground="#6578a0")
-EventCanvas2.grid(row=2, column=1, columnspan=3, pady=10)
-
-DeleteButton2 = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
-DeleteButton2.config(fg="#8b0000")
-DeleteButton2.grid(row=2, column=4)
+eventcanvas1.config(bd=3, relief="ridge", highlightbackground="#6578a0")
+eventcanvas1.grid(row=1, column=1, columnspan=3, pady=10)
 
 
-#creates 4 separate 
-def popupwindow():
-   EventNameInput = simpledialog.askstring("Add New Event", "Event Name:")
-   DateInput = simpledialog.askstring("Add New Event", "Date (Month, Day, Year):")
-   TimeInput = simpledialog.askstring("Add New Event", "Time (Include am or pm):")
-   TimeNeededInput = simpledialog.askstring("Add New Event", "Time Needed (In Minutes):", parent = root)
+deletebutton1.config(fg="#8b0000", command = deletetext1)
+deletebutton1.grid(row=1, column=4)
 
 
-#creates new file using pop-up window
-def saveeventstofile():
-    f = filedialog.asksaveasfile(mode="a", defaultextension=".txt")
-    if f is None:
-        return
-
-    f.write(EventNameInput)
-    f.close
+eventlabel1.config(font =("Franklin Gothic", "30"), fg="#243c6a")
+eventlabel1.grid(row=1, column=1)
 
 
-def write_File (text_File):
-    file = open("Events.txt", "a")
+timeanddatelabel1.config(font =("Franklin Gothic", "30"), fg="#243c6a")
+timeanddatelabel1.grid(row=1, column=2)
 
 
-AddNewEventButton = tk.Button(root, text ="Add New Event", command = popupwindow, fg = "#243c6a", relief = "groove")
-AddNewEventButton.config(width=45, height=2, bd=3, relief="ridge", highlightbackground="#6578a0", font =("Franklin Gothic", "30"))
-AddNewEventButton.grid(row=3, column=1, columnspan=3, sticky=W)
+timeneededlabel1.config(font =("Franklin Gothic", "30"), fg="#243c6a")
+timeneededlabel1.grid(row=1, column=3)
+
+
+addneweventbutton.config(width=45, height=2, bd=3, relief="ridge", highlightbackground="#6578a0", font =("Franklin Gothic", "30"), command = popupwindow)
+addneweventbutton.grid(row=10, column=1, columnspan=3, sticky=W)
 
 
 root.mainloop()
--- Unit 1 Project/TaskBarRoughCopy.py
+++ Unit 1 Project/TaskBarRoughCopy.py
@@ -12,49 +12,11 @@ this is to use Object Oriented Design.
 The effect is that you need to store all variables as lists
 '''
 
-#VARIABLES
 #This will be a temporary data holder 
-data = ["","","",""] #Creates an empty list
-
-
-#FUNCTIONS
-#creates 4 separate 
-def popupwindow():
-  #There is a nice opportunity for some while loops to error check
-  #You can check the type of a variable using the type command
-  
-   data[0]= simpledialog.askstring("Add New Event", "Event Name:")    
-   data[1]= simpledialog.askstring("Add New Event", "Date (Month, Day, Year):")
-   data[2] = simpledialog.askstring("Add New Event", "Time (Include am or pm):")
-   data[3] = simpledialog.askstring("Add New Event", "Time Needed (In Minutes):", parent = root)
-
-
-#creates new file using pop-up window
-def saveeventstofile():
-    f = filedialog.asksaveasfile(mode="a", defaultextension=".txt")
-    if f is None:
-        return
-
-    f.write(EventNameInput)
-    f.close
-
-
-def write_File (text_File):
-    file = open("Events.txt", "a")
+data = [] #Creates an empty list
 
 
 
-#deletes whole event bar
-def deletetext():
-    EventLabel.destroy()
-    TimeAndDateLabel.destroy()
-    TimeNeededLabel.destroy()
-    EventCanvas1.destroy()
-    DeleteButton1.destroy()
-
-
-
-#WINDOW CONSTRUCTION
 print("Start Program")
 
 root = tk.Tk()
@@ -70,6 +32,8 @@ canvas.grid(row=0, column=0)
 photoimage = ImageTk.PhotoImage(file="SmallUCCLogo.png")
 canvas.create_image(150, 50, image=photoimage)
 
+
+
 today = date.today()
 
 d2 = today.strftime("%B %d, %Y")
@@ -78,6 +42,8 @@ d2 = today.strftime("%B %d, %Y")
 todaysdateLabel = tk.Label(root, text = d2)
 todaysdateLabel.config(bg = "#243c6a", fg = "#6578a0", font =("Franklin Gothic", "30"))
 todaysdateLabel.grid(row=0, column=4)
+
+
 
 today = date.today()
 
@@ -96,6 +62,14 @@ TaskBarLabel.grid(row=0, column=2)
 EventCanvas1 = Canvas(root, width=800, height=100)
 EventCanvas1.config(bd=3, relief="ridge", highlightbackground="#6578a0")
 EventCanvas1.grid(row=1, column=1, columnspan=3, pady=10)
+
+#deletes whole event bar
+def deletetext():
+    EventLabel.destroy()
+    TimeAndDateLabel.destroy()
+    TimeNeededLabel.destroy()
+    EventCanvas1.destroy()
+    DeleteButton1.destroy()
 
 DeleteButton1 = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
 DeleteButton1.config(fg="#8b0000", command = deletetext)
@@ -120,6 +94,28 @@ EventCanvas2.grid(row=2, column=1, columnspan=3, pady=10)
 DeleteButton2 = tk.Button(root, text = "DELETE", highlightbackground="#8b0000", highlightthickness=20)
 DeleteButton2.config(fg="#8b0000")
 DeleteButton2.grid(row=2, column=4)
+
+
+#creates 4 separate 
+def popupwindow():
+   EventNameInput = simpledialog.askstring("Add New Event", "Event Name:")
+   DateInput = simpledialog.askstring("Add New Event", "Date (Month, Day, Year):")
+   TimeInput = simpledialog.askstring("Add New Event", "Time (Include am or pm):")
+   TimeNeededInput = simpledialog.askstring("Add New Event", "Time Needed (In Minutes):", parent = root)
+
+
+#creates new file using pop-up window
+def saveeventstofile():
+    f = filedialog.asksaveasfile(mode="a", defaultextension=".txt")
+    if f is None:
+        return
+
+    f.write(EventNameInput)
+    f.close
+
+
+def write_File (text_File):
+    file = open("Events.txt", "a")
 
 
 AddNewEventButton = tk.Button(root, text ="Add New Event", command = popupwindow, fg = "#243c6a", relief = "groove")
